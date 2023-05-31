867ff9384059e1fff69f4436dc54a6906cae578e
amoghtelkar <amoghtelkar96@gmail.com>
2023-05-31 17:35:16 +0530

    added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// curry method to perform addition operation
 export const add = (num1) => {
     return (num2) => {
         if (!num2) {
@@ -6,6 +7,7 @@ export const add = (num1) => {
         return add(num1 + num2);
     };
 }
+// curry method to perform subtract operation
 export const sub = (num1) => {
     return (num2) => {
         if (!num2) {
@@ -14,6 +16,7 @@ export const sub = (num1) => {
         return sub(num1 - num2);
     };
 }
+// curry method to perform multiplicaton operation
 export const mul = (num1) => {
     return (num2) => {
         if (!num2) {
@@ -22,6 +25,7 @@ export const mul = (num1) => {
         return mul(num1 * num2);
     };
 }
+// curry method to perform division operation
 export const div = (num1) => {
     return (num2) => {
         if (!num2) {
@@ -30,6 +34,7 @@ export const div = (num1) => {
         return div(num1 / num2);
     };
 }
+// curry method to perform modulus operation
 export const mod = (num1) => {
     return (num2) => {
         if (!num2) {
